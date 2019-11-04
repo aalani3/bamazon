d7219a1586dd01bf8fef1f553d413f3df0f22d7b
Ali Alani <ahasaad@gmail.com>
2019-11-04 13:22:01 -0600

    1

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,52 +1,115 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
-//the connection variable to sync with a MySQL database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-
-    user: "root",
-    password: "Ameen-2018",
-    database: "bamazon_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Ameen-2018",
+  database: "bamazon"
 });
 
-// establish connection
-connection.connect(function (err) {
-    if (err) throw err;
-
-    loadProducts();
+connection.connect(function(err) {
+  if (err) throw err;
+  displayItemsForSale();
 });
 
-function loadProducts() {
+function displayItemsForSale() {
+  connection.query(
+    "select item_id,product_name, price, stock_quantity from products where stock_quantity>0",
+    function(err, res) {
+      if (err) throw err;
+      console.log("Id \t Name \t Price \t Quantity\n");
+      for (var i = 0; i < res.length; i++) {
+        console.log(
+          res[i].item_id +
+            "\t" +
+            res[i].product_name +
+            "\t" +
+            res[i].price +
+            "\t" +
+            res[i].stock_quantity +
+            "\n"
+        );
+      }
+      promptQuestions(res.length);
+    }
+  );
+}
 
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-
-        console.table(res);
-
-        promptItems(res);
+function promptQuestions(length) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "purcase_item_id",
+        message:
+          "Enter the Item Id of the product you would like to buy??? 'Press C to Exit'"
+      }
+    ])
+    .then(function(answer) {
+      var purchaseItemId = answer.purcase_item_id;
+      if (purchaseItemId.toUpperCase() === "C") {
+        process.exit();
+      }
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "quantity",
+            message: "how many units of the product would you like to buy???"
+          }
+        ])
+        .then(function(answer) {
+          if (
+            purchaseItemId > length + 1 ||
+            isNaN(purchaseItemId) ||
+            isNaN(answer.quantity)
+          ) {
+            console.log("invalid Input");
+            if (purchaseItemId > length + 1 || isNaN(purchaseItemId)) {
+              console.log("The item id is not valid");
+            }
+            if (isNaN(answer.quantity)) {
+              console.log("Invalid quantity");
+            }
+            // connection.end();
+            displayItemsForSale();
+          } else {
+            connection.query(
+              "select stock_quantity, price from products where item_id = ?",
+              [purchaseItemId],
+              function(err, res) {
+                if (err) throw err;
+                if (answer.quantity > res[0].stock_quantity) {
+                  console.log("Insufficient quantity!");
+                } else {
+                  var updateQuantity =
+                    res[0].stock_quantity - parseFloat(answer.quantity);
+                  connection.query(
+                    "update products set ? where ?",
+                    [
+                      {
+                        stock_quantity: updateQuantity
+                      },
+                      {
+                        item_id: purchaseItemId
+                      }
+                    ],
+                    function(err, res) {
+                      if (err) throw err;
+                    }
+                  );
+                  var totalCost = res[0].price * answer.quantity;
+                  console.log(
+                    "The total price of the purchase : " + totalCost.toFixed(2)
+                  );
+                }
+                // connection.end();
+                displayItemsForSale();
+              }
+            );
+          }
+        });
     });
 }
-
-
-function promptItems() {
-
-    inquirer
-        .prompt(
-            {
-                type: "input",
-                name: "ProductId",
-                message: "What is the item's Id you would you like to purchase?",
-                validate: function (value) {
-                    if (!isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-        );
-
-}
-
--- schema.sql
+++ schema.sql
@@ -1,29 +1,31 @@
-DROP DATABASE IF EXISTS bamazon_db;
-CREATE DATABASE bamazon_db;
+DROP DATABASE IF EXISTS bamazon;
 
-CREATE TABLE products(
-item_id INT AUTO_INCREMENT NOT NULL,
-product_name VARCHAR (50) NOT NULL,
-department VARCHAR (50) NOT NULL,
-price DECIMAL (10,2) NULL,
-quantity INT (11) NOT NULL,
-PRIMARY KEY (item_id)
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NULL,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  price DECIMAL(10, 2) NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (id)
 );
-
-USE bamazon_db;
 
 SELECT * FROM products;
 
-INSERT INTO products(product_name, department, price, quantity)
-VALUES ("Green tea", "Food and Beverages", 12, 120),
-	   ("Tennis Racket", "Sports", 75, 50),
-       ("Z t-shirt", "Apparel", 20, 100),
-       ("Piano", "Music", 12, 500),
-       ("Y chinos", "Apparel", 45, 75),
-       ("Q2 shoes", "Footware", 100, 100),
-       ("Pampers", "Baby", 25.50, 200),
-       ("Learning JS", "Books", 32.30, 60),
-       ("Madden 20", "Video Games", 58, 110),
-       ("Coffee", "Food and Beverages", 14, 220),
-       ("cleaning Towels", "Auto", 9, 250);
+INSERT INTO products(item_id, product_name, department, price, quantity)
+VALUES (01,"Green tea", "Food and Beverages", 12, 120),
+	   (02,"Tennis Racket", "Sports", 75, 50),
+       (03,"Z t-shirt", "Apparel", 20, 100),
+       (04,"Piano", "Music", 400, 20),
+       (05,"Y chinos", "Apparel", 45, 75),
+       (06,"Q2 shoes", "Footware", 100, 100),
+       (07,"Pampers", "Baby", 25.50, 200),
+       (08,"Learning JS", "Books", 32.30, 60),
+       (09,"Madden 20", "Video Games", 58, 110),
+       (10,"Coffee", "Food and Beverages", 14, 220);
+    
        
